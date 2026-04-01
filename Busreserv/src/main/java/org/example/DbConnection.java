22df1aafdabf29f4938fb2d468a29718325bfd0f
Admin <arunvengatesh17356@gmail.com>
2026-04-01 22:18:07 +0530

    first change for bus reservation

--- Busreserv/src/main/java/org/example/DbConnection.java
+++ Busreserv/src/main/java/org/example/DbConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConnection {
     private static final String url="jdbc:mysql://localhost:3306/Newbusresv1";
     private static final String userName="root";
-    private static final String password="Arun@2002";
+    private static final String password="xyz";
     public static Connection getconnection() throws SQLException {
         return DriverManager.getConnection(url,userName,password);
     }
